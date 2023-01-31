609e142c3b44c497ea25a4a8f75fa4f81c52afa6
binh.phan <phanthanhbinh296@gmail.com>
2023-01-31 22:57:11 +0700

    feat: arent test - my profile screen

--- src/components/DiaryModal/modal-styled.tsx
+++ src/components/DiaryModal/modal-styled.tsx
@@ -1,20 +1,3 @@
 import styled from 'styled-components';
 
-export const ModalStyled = styled.div`
-  .draggble-setting-column {
-    cursor: move !important;
-    .ant-list-item-action {
-      .ant-btn-icon-only {
-        .anticon {
-          vertical-align: 0.325em;
-        }
-      }
-      .ant-list-item-action-split {
-        width: 0px;
-      }
-    }
-  }
-  .ant-select-auto-complete {
-    margin-bottom: 10px;
-  }
-`;
+export const ModalStyled = styled.div``;
